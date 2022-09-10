3741394d16b858d52b2eaf6660b17e815d1f188b
FukeKazki <kazkichi0906@gmail.com>
2022-09-10 17:14:41 +0900

    prettier

--- App.tsx
+++ App.tsx
@@ -1,98 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+	Image,
+	Platform,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View
+} from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import * as Sharing from 'expo-sharing';
-import * as ImageManipulator from "expo-image-manipulator";
+import * as ImageManipulator from 'expo-image-manipulator';
 import { useState } from 'react';
 
 export default function App() {
-  const [selectedImage, setSelectedImage] = useState<ImagePicker.ImageInfo>();
+	const [selectedImage, setSelectedImage] = useState<ImagePicker.ImageInfo>();
 
-  const openImagePicker = async () => {
-    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+	const openImagePicker = async () => {
+		const permissionResult =
+			await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    if (!permissionResult.granted) {
-      alert('権限がないよ！')
-      return;
-    }
-    // 画像選択ダイアログを開く
-    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+		if (!permissionResult.granted) {
+			alert('権限がないよ！');
+			return;
+		}
+		// 画像選択ダイアログを開く
+		const pickerResult = await ImagePicker.launchImageLibraryAsync();
 
-    if (pickerResult.cancelled) return;
+		if (pickerResult.cancelled) return;
 
-    setSelectedImage(pickerResult);
-  }
+		setSelectedImage(pickerResult);
+	};
 
-  const openShareDialog = async () => {
-    if (!selectedImage) return;
-    if (Platform.OS === 'web') return;
+	const openShareDialog = async () => {
+		if (!selectedImage) return;
+		if (Platform.OS === 'web') return;
 
-    // uriから画像化
-    const imageTmp = await ImageManipulator.manipulateAsync(selectedImage.uri);
-    // シェアダイアログを開く
-    await Sharing.shareAsync(imageTmp.uri);
-  }
+		// uriから画像化
+		const imageTmp = await ImageManipulator.manipulateAsync(
+			selectedImage.uri
+		);
+		// シェアダイアログを開く
+		await Sharing.shareAsync(imageTmp.uri);
+	};
 
-  return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
-      <Text style={styles.instructions}>Hello Expo with @fukke0906</Text>
-      <TouchableOpacity
-        onPress={openImagePicker}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Pick a Photo</Text>
-      </TouchableOpacity>
+	return (
+		<View style={styles.container}>
+			<StatusBar style='auto' />
+			<Image
+				source={{ uri: 'https://i.imgur.com/TkIrScD.png' }}
+				style={styles.logo}
+			/>
+			<Text style={styles.instructions}>Hello Expo with @fukke0906</Text>
+			<TouchableOpacity onPress={openImagePicker} style={styles.button}>
+				<Text style={styles.buttonText}>Pick a Photo</Text>
+			</TouchableOpacity>
 
-      {selectedImage && (
-        <View>
-          <Image
-            source={{ uri: selectedImage.uri }}
-            style={styles.thumbnail}
-          />
-          <TouchableOpacity
-            onPress={openShareDialog}
-            style={styles.button}
-          >
-            <Text style={styles.buttonText}>Share</Text>
-          </TouchableOpacity>
-        </View>
-      )}
-    </View>
-  );
+			{selectedImage && (
+				<View>
+					<Image
+						source={{ uri: selectedImage.uri }}
+						style={styles.thumbnail}
+					/>
+					<TouchableOpacity
+						onPress={openShareDialog}
+						style={styles.button}
+					>
+						<Text style={styles.buttonText}>Share</Text>
+					</TouchableOpacity>
+				</View>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 10
-  },
-  instructions: {
-    color: '#888',
-    fontSize: 18,
-    marginHorizontal: 15,
-  },
-  button: {
-    marginTop: 10,
-    backgroundColor: 'blue',
-    padding: 20,
-    borderRadius: 5
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 20
-  },
-  thumbnail: {
-    width: 300,
-    height: 200,
-    resizeMode: 'contain'
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	logo: {
+		width: 305,
+		height: 159,
+		marginBottom: 10
+	},
+	instructions: {
+		color: '#888',
+		fontSize: 18,
+		marginHorizontal: 15
+	},
+	button: {
+		marginTop: 10,
+		backgroundColor: 'blue',
+		padding: 20,
+		borderRadius: 5
+	},
+	buttonText: {
+		color: '#fff',
+		fontSize: 20
+	},
+	thumbnail: {
+		width: 300,
+		height: 200,
+		resizeMode: 'contain'
+	}
 });
